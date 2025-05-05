fddc90709032bb4285fe3248b8af14e8d9fc13e3
Mohid <mohid.jillani@gmail.com>
2025-05-05 16:03:11 +0500

    Initialization

---
+++ app/src/main/java/com/example/scheduler/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.scheduler;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.scheduler.Tabs.NotificationFrag;
+import com.example.scheduler.Tabs.PastFrag;
+import com.example.scheduler.Tabs.ProfileFrag;
+import com.example.scheduler.Tabs.ScheduleFrag;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(FragmentActivity fa) {
+        super(fa);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new ScheduleFrag();
+            case 1: return new PastFrag();
+            case 2: return new NotificationFrag();
+            case 3: return new ProfileFrag();
+            default: return new ScheduleFrag();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
